3cda3b0550fd76ce172929c2d923dd8748bcb374
Zet2x <dmamontov33@gmail.com>
2024-04-12 20:24:46 +0300

    first commit

---
+++ src/component/Contact.js
@@ -0,0 +1,64 @@
+import { useState } from "react"
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import contactImg from '../component/img/contact-img.svg';
+
+export const Contact = () => {
+    const formInitialDetails = {
+        firstName: '',
+        lastName: '',
+        email: '',
+        phone: '',
+        message: '',
+    }
+
+    const [formDetails, setFormDetails] = useState(formInitialDetails);
+    const [buttonText, setButtonText] = useState('Отправить');
+    const [status, setStatus] = useState({});
+    const onFormUpdate = (category, value) => {
+        setFormDetails({
+            ...formDetails,
+            [category]: value
+        })
+    }
+
+    return (
+        <section className="contact" id="connect">
+            <Container>
+                <Row className="align-items-center">
+                    <Col md={6}>
+                        <img src={contactImg} alt="Свяжитесь с нами!" />
+                    </Col>
+                    <Col md={6}>
+                        <h2>Связаться</h2>
+                        <form>
+                            <Row>
+                                <Col sm={6} className="px-1">
+                                    <input type="text" value={formDetails.firstName} placeholder="Имя" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
+                                </Col>
+                                <Col sm={6} className="px-1">
+                                    <input type="text" value={formDetails.lastName} placeholder="Фамилия" onChange={(e) => onFormUpdate('lastName', e.target.value)} />
+                                </Col>
+                                <Col sm={6} className="px-1">
+                                    <input type="email" value={formDetails.email} placeholder="Электронная почта" onChange={(e) => onFormUpdate('email', e.target.value)} />
+                                </Col>
+                                <Col sm={6} className="px-1">
+                                    <input type="tel" value={formDetails.phone} placeholder="Номер телефона" onChange={(e) => onFormUpdate('phone', e.target.value)} />
+                                </Col>
+                                <Col>
+                                    <textarea row="6" value={formDetails.message} placeholder="Сообщение" onChange={(e) => onFormUpdate('message', e.target.value)} />
+                                    <button type="submit"><span>{buttonText}</span></button>
+                                </Col>
+                                {
+                                    status.message &&
+                                    <Col>
+                                        <p className={status.sucess === false ? "danger" : "sucess"}>{ status.message }</p>
+                                    </Col>
+                                }
+                            </Row>
+                        </form>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    )
+}
---
+++ src/component/Projects.js
@@ -0,0 +1,87 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCards } from "./ProjectCards";
+import colorSharp2 from '../component/img/color-sharp2.png'
+import projImg1 from "../component/img/project-img1.png";
+import projImg2 from "../component/img/project-img2.png";
+import projImg3 from "../component/img/project-img3.png";
+
+export const Projects = () => {
+    const projects = [
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg1,
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg2,
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg3,
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg1,
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg2,
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg3,
+        },
+    ];
+    return (
+        <section className="project" id="project">
+            <Container>
+                <Row>
+                    <Col>
+                        <h2>Проекты</h2>
+                        <p>Loremdsjhgfgslafdafdafdsvhasjbadshf</p>
+                        <Tab.Container id="projects-tabs" defaultActiveKey='first' className="">
+                            <Nav variant="pills" defaultActiveKey="first" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                                <Nav.Item>
+                                    <Nav.Link eventKey="first">Первая вкладка</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="second">Вторая вкладка</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="third" disabled>
+                                        Третья вкладка
+                                    </Nav.Link>
+                                </Nav.Item>
+                            </Nav>
+                            <Tab.Content>
+                                <Tab.Pane eventKey="first">
+                                    <Row>
+                                        {
+                                            projects.map((project, index) => {
+                                                return (
+                                                    <ProjectCards
+                                                        key={index}
+                                                        {...project}
+                                                    />
+                                                )
+                                            })
+                                        }
+                                    </Row>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="second">Loren Ipsum</Tab.Pane>
+                                <Tab.Pane eventKey="third">Loren Ipsum</Tab.Pane>
+                            </Tab.Content>
+                        </Tab.Container>
+                    </Col>
+                </Row>
+            </Container>
+            <img className="background-image-right" src={colorSharp2} />
+        </section>
+    )
+}
